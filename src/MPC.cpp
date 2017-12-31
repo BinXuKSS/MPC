dfdd772ba22aa625b4cdde993fbab93700f41a59
BinXu <15962108591@163.com>
2017-12-31 22:19:00 +0800

    update

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -102,7 +102,11 @@ class FG_eval {
 		AD<double> epsi0 = vars[epsi_start + t - 1];      
 		// Only consider the actuation at time t.      
 		AD<double> delta0 = vars[delta_start + t - 1];      
-		AD<double> a0 = vars[a_start + t - 1];   
+		AD<double> a0 = vars[a_start + t - 1];  
+		if (t > 1) {   // consider for actuator latency
+        	a0 = vars[a_start + t - 2];
+        	delta0 = vars[delta_start + t - 2];
+      }
 		
         AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);      
 		AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));    
